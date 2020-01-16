d86d08d333c1407443cd2f87ab0b9da8eca9e590
Shagun Mistry <shagun.mistry@hotmail.com>
2020-01-16 14:54:04 -0500

    Update Challenge trigger time

--- startup.js
+++ startup.js
@@ -8,7 +8,7 @@ const momentTimezone = require('moment-timezone');
 
 let webserv, client;
 
-exports.run = (c)=>{
+exports.run = (c) => {
 	client = c
 	webserv = require('./express')
 	webserv.init(client)
@@ -23,16 +23,20 @@ async function dailyChallengeHandler() {
 	let date = new Date();
 	let estDate = moment.tz('America/New_York').toDate();
 
-	if(estDate.getHours() === 12 && estDate.getMinutes() === 0 && estDate.toLocaleString().slice(-2) === 'PM') {
-		console.log(date.getHours(), date.getMinutes(), lastTrigger, lastTrigger != null ? date.getTime() - lastTrigger.getTime() : '')
-		if(lastTrigger == null || date.getTime() - lastTrigger.getTime() > 80000000) {
-			for(let guild of client.guilds.array()) {
-				let channel = await guild.channels.find(x => x.name.toLowerCase() === 'daily-challenge');
+	if (
+		(estDate.getHours() === 9 || estDate.getHours === 09)
+		&& estDate.getMinutes() === 00
+		&& estDate.toLocaleString().slice(-2) === 'AM'
+	) {
+		console.log(estDate.getHours(), estDate.getMinutes(), lastTrigger, lastTrigger != null ? estDate.getTime() - lastTrigger.getTime() : '')
+		if (lastTrigger == null || estDate.getTime() - lastTrigger.getTime() > 80000000) {
+			for (let guild of client.guilds.array()) {
+				let channel = await guild.channels.find(x => x.name.toLowerCase() === 'test-channel');
 				let theChosenMember = guild.members.random();
-				if(channel == null) return
+				if (channel == null) return
 				let res = await channel.send(`**${date.toString().match(/.*? .*? .*? /)[0].trim()}:** <@${theChosenMember.id}> => ${(await dbcmds.getChallenge()).message}`);
-				console.log(`Daily challenge sent at ${date.toLocaleString()}`)
-				lastTrigger = date
+				console.log(`Daily challenge sent at ${estDate.toLocaleString()}`)
+				lastTrigger = estDate
 			}
 		}
 	}
